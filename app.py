cb868dec0d80d8af4f4a5e9b776d099b947c7e9c
jordanrob709 <jordanrob709@gmail.com>
2019-01-18 11:48:02 +0300

    this commit is made to address the error in the heroku logs where db is not defined

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 api=Api(app)
 app.secret_key='jordan'
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt=JWT(app, authenticate, identity) #/auth
 
